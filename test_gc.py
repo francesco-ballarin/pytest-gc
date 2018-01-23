f01842b66357e04e4bf0f412ed2366adff039e61
Victor Titor <vtitor.edumix@gmail.com>
2018-01-23 20:10:39 +0200

    Add linting

--- test_gc.py
+++ test_gc.py
@@ -1,11 +1,11 @@
 pytest_plugins = "pytester",
 
 
-def test_switch(testdir):
+def test_disable(testdir):
     testdir.makepyfile("""
     import gc
-    
-    def test_switch():
+
+    def test_disable():
         assert not gc.isenabled()
     """)
     testdir.runpytest('--gc-disable').assert_outcomes(passed=1)
