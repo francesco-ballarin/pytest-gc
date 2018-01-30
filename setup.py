b3a7a1ed1901029e7d861864276f6d7987a9adc0
Victor Titor <vtitor.edumix@gmail.com>
2018-01-31 01:28:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 setup(
     name='pytest-gc',
     description='The garbage collector plugin for py.test',
+    long_description=open('README.rst').read(),
     version='0.0.1',
     author='Victor Titor',
+    author_email='vtitor.edumix@gmail.com',
     url='https://github.com/vtitor/pytest-gc',
     packages=['pytest_gc'],
     entry_points={'pytest11': ['gc = pytest_gc:PluginLoader']},
