afddf9d4884c2a0bfe0b63f94cbfc4b120392b63
Victor Titor <vtitor.edumix@gmail.com>
2018-05-22 18:05:01 +0300

    Fix formatting using black

--- test_gc.py
+++ test_gc.py
@@ -1,22 +1,24 @@
-pytest_plugins = 'pytester',
+pytest_plugins = ("pytester",)
 
 
 def test_disable(testdir):
-    testdir.makepyfile("""
+    test_file = """
     import gc
 
     def test_disable():
         assert not gc.isenabled()
-    """)
-    testdir.runpytest('--gc-disable').assert_outcomes(passed=1)
+    """
+    testdir.makepyfile(test_file)
+    testdir.runpytest("--gc-disable").assert_outcomes(passed=1)
 
 
 def test_threshold(testdir):
     threshold = 1, 2, 3
-    testdir.makepyfile("""
+    test_file = """
     import gc
 
     def test_threshold():
         assert gc.get_threshold() == {}
-    """.format(threshold))
-    testdir.runpytest('--gc-threshold', *threshold).assert_outcomes(passed=1)
+    """
+    testdir.makepyfile(test_file.format(threshold))
+    testdir.runpytest("--gc-threshold", *threshold).assert_outcomes(passed=1)
